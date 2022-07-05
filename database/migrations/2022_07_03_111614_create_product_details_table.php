9d3e256cb22ac4feda268f0e882b9c6a920a4dc8
Victor <vsr.romero@gmail.com>
2022-07-05 02:02:04 +0100

    created site contact table, suppliers table, products table, products details table

---
+++ database/migrations/2022_07_03_111614_create_product_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->text('description');
+            $table->float('wheight', 8 , 2)->nullable();
+            $table->float('height', 8 , 2)->nullable();
+            $table->float('depth', 8 , 2)->nullable();
+            $table->timestamps();
+
+            //relationship constraint - foreign key
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->unique('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_details');
+    }
+}
