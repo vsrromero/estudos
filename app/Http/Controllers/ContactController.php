86dd8664d742f0b6c2da178a7f67b56b17c0126c
Victor <vsr.romero@gmail.com>
2022-07-12 01:08:31 +0100

    Persist form on DB

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContact;
 
 class ContactController extends Controller
 {
-    public function contact(){
+    public function contact(Request $request){
         //return view and send 'title' as variable to parameter $title
+        /*
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('name');
+        */
+
+
+        $insert = new SiteContact();
+        $insert->name = $request->input('name');
+        $insert->phone = $request->input('phone');
+        $insert->email = $request->input('email');
+        $insert->contact_reason = $request->input('reasonContact');
+        $insert->message = $request->input('message');
+        $insert->save();
+
+
+
         return view('website.contact' , ['title' => 'Contact']);
     }
 }
